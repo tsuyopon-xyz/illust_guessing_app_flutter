4746d372562853c2b303d2ebacaaead1d15e9de8
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-03-05 17:27:05 +0900

    Created selected_quiz_progres_provider and test for it.

--- lib/src/providers/selected_quiz_progress_provider.dart
+++ lib/src/providers/selected_quiz_progress_provider.dart
@@ -5,27 +5,33 @@ import '../models/selected_quiz_progress.dart';
 
 class _SelectedQuizProgressNotifier
     extends StateNotifier<SelectedQuizProgress> {
-  // Todo リストを空のリストとして初期化します。
   _SelectedQuizProgressNotifier() : super(SelectedQuizProgress());
 
   // Todo の追加
   void selectQuiz(Quiz quiz) {
-    // ステート自体もイミュータブルなため、`state.add(todo)`
-    // のような操作はできません。
-    // 代わりに、既存 Todo と新規 Todo を含む新しいリストを作成します。
-    // Dart のスプレッド演算子を使うと便利ですよ!
-    int firstChapterOrder = quiz.chapters.first.order;
+    int startChapterIndex = 0;
     state = SelectedQuizProgress(
-        quiz: quiz, currentChapterOrder: firstChapterOrder);
-    // `notifyListeners` などのメソッドを呼ぶ必要はありません。
-    // `state =` により必要なときに UI側 に通知が届き、ウィジェットが更新されます。
+        quiz: quiz, currentChapterIndex: startChapterIndex);
+  }
+
+  bool hasNextChapter() {
+    return state.hasNextChapter();
+  }
+
+  void goToNextChapter() {
+    if (!state.hasNextChapter()) {
+      return;
+    }
+
+    int oldChapterIndex = state.currentChapterIndex!;
+    int newChapterIndex = oldChapterIndex + 1;
+    state = state.copyWith(currentChapterIndex: newChapterIndex);
   }
 }
 
 // 最後に TodosNotifier のインスタンスを値に持つ StateNotifierProvider を作成し、
 // UI 側から Todo リストを操作することを可能にします。
-final selectedQuizProgressProvider =
-    StateNotifierProvider<_SelectedQuizProgressNotifier, SelectedQuizProgress>(
-        (ref) {
+final selectedQuizProgressProvider = StateNotifierProvider.autoDispose<
+    _SelectedQuizProgressNotifier, SelectedQuizProgress>((_) {
   return _SelectedQuizProgressNotifier();
 });
--- test/src/providers/selected_quiz_progress_provider_test.dart
+++ test/src/providers/selected_quiz_progress_provider_test.dart
@@ -1,27 +1,82 @@
+import 'dart:convert';
+import 'package:mockito/mockito.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:illust_guessing_app/src/providers/selected_quiz_progress_provider.dart';
+import 'package:illust_guessing_app/src/providers/selected_quiz_progress_provider.dart'
+    show selectedQuizProgressProvider;
 import 'package:illust_guessing_app/src/models/quiz.dart';
+import 'package:illust_guessing_app/src/models/selected_quiz_progress.dart';
+import 'package:illust_guessing_app/src/loaders/json_loader.dart';
 
+// mockito を使ってプロバイダによる Listener への通知を追跡する
+class Listener extends Mock {
+  void call(SelectedQuizProgress? previous, SelectedQuizProgress value);
+}
+
+// Providerテストの参考記事
+// https://zuma-lab.com/posts/flutter-mockito-di-unit-test
+// https://riverpod.dev/ja/docs/cookbooks/testing
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  late Quiz _quiz;
 
-  test('Get all quiz instances in a list.', () async {
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    final jsonLoader = JsonLoader();
+    final String jsonString = await jsonLoader.loadQuizzes();
+    var data = json.decode(jsonString);
+    var singleQuizJson = data[0];
+    _quiz = Quiz.fromJson(singleQuizJson);
+  });
+
+  test('Check behavior of selectedQuizProgressProvider', () {
     final container = ProviderContainer();
-    addTearDown(() {
-      print("Finish this test");
-      addTearDown(container.dispose);
+    final listener = Listener();
+    addTearDown(container.dispose);
+
+    container.listen<SelectedQuizProgress>(
+        selectedQuizProgressProvider, listener,
+        fireImmediately: true);
+
+    // 初期化時のstateの変化をテスト
+    verify(listener(null, SelectedQuizProgress())).called(1);
+    verifyNoMoreInteractions(listener);
+
+    // selectQuizメソッド実行時のstateの変化をテスト
+    var notifier = container.read(selectedQuizProgressProvider.notifier);
+    notifier.selectQuiz(_quiz);
+
+    verify(listener(SelectedQuizProgress(),
+            SelectedQuizProgress(quiz: _quiz, currentChapterIndex: 0)))
+        .called(1);
+    verifyNoMoreInteractions(listener);
+
+    // goToNextChapterメソッド実行時のstateの変化をテスト
+    int lastIndex = _quiz.chapters.length - 1;
+    _quiz.chapters.asMap().forEach((index, _) {
+      bool hasNext = notifier.hasNextChapter();
+      if (index < lastIndex) {
+        expect(hasNext, true);
+        notifier.goToNextChapter();
+        verify(listener(
+                SelectedQuizProgress(quiz: _quiz, currentChapterIndex: index),
+                SelectedQuizProgress(
+                    quiz: _quiz, currentChapterIndex: index + 1)))
+            .called(1);
+      } else {
+        expect(hasNext, false);
+        notifier.goToNextChapter();
+        verifyNever(listener(
+            SelectedQuizProgress(quiz: _quiz, currentChapterIndex: index),
+            SelectedQuizProgress(quiz: _quiz, currentChapterIndex: index + 1)));
+      }
+
+      verifyNoMoreInteractions(listener);
     });
 
-    AsyncValue<List<Quiz>> asyncValue = container.read(quizListProvider);
-    expect(asyncValue, const AsyncValue<List<Quiz>>.loading());
+    // 最終的な値
+    SelectedQuizProgress sqp = notifier.state;
 
-    await container.read(quizListProvider.future);
-
-    List<Quiz> quizList = container.read(quizListProvider).value!;
-
-    expect(quizList.isNotEmpty, true);
-    expect(quizList[0].id, 1);
-    expect(quizList[0].chapters.length, 4);
+    expect(sqp.currentChapterIndex, sqp.quiz!.chapters.length - 1);
+    expect(sqp.hasNextChapter(), false);
   });
 }
