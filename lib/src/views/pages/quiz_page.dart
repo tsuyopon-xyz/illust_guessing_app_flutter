409383b38f8a91eb4ac65953e78720b145f0efb4
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-03-11 02:38:08 +0900

    WIP touch problem.

--- lib/src/views/pages/quiz_page.dart
+++ lib/src/views/pages/quiz_page.dart
@@ -37,8 +37,22 @@ class _QuizImageView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var chapters = quiz.chapters;
+    if (chapters == null || chapters.isEmpty) {
+      return const Center(
+        child: Text('Chapter is not found'),
+      );
+    }
+
     final chapterIndex = useState(0);
-    final chapter = quiz.chapters?.elementAt(chapterIndex.value);
+    if (chapters.length <= chapterIndex.value) {
+      print("Finished Quiz!");
+      return const Center(
+        child: Text('Finished Quiz!'),
+      );
+    }
+
+    final chapter = chapters[chapterIndex.value];
     if (chapter == null) {
       return const Center(
         child: Text('Chapter is not found'),
@@ -46,7 +60,50 @@ class _QuizImageView extends HookConsumerWidget {
     }
 
     return Center(
-      child: Image.asset(chapter.imagePath),
+      child: GestureDetector(
+        onTapDown: (TapDownDetails details) {
+          bool isCorrect = _isCorrectArea(details.localPosition, chapter);
+          if (isCorrect) {
+            chapterIndex.value++;
+            print("chapterIndex : $chapterIndex");
+          }
+        },
+        child: Stack(
+          children: [
+            Image.asset(chapter.imagePath),
+
+            // To visible correct area.
+            Positioned(
+              top: chapter.correctAreaX.toDouble(),
+              left: chapter.correctAreaY.toDouble(),
+              width: chapter.correctAreaWidth.toDouble(),
+              height: chapter.correctAreaHeight.toDouble(),
+              child: Container(color: Colors.red.shade500.withOpacity(0.5)),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  bool _isCorrectArea(Offset localPosition, Chapter chapter) {
+    double left = chapter.correctAreaX.toDouble();
+    double top = chapter.correctAreaY.toDouble();
+    double right = left + chapter.correctAreaWidth.toDouble();
+    double bottom = top + chapter.correctAreaHeight.toDouble();
+    double touchX = localPosition.dx;
+    double touchY = localPosition.dy;
+
+    // TODO: touch positionが期待している値と微妙に異なっている部分の調査。
+    // 参考: left(70.0)付近をタッチした時の値が32.0になってしまっている
+    // left: 70.0, top: 30.0, right: 150.0, bottom: 130.0,
+    // touchX: 32.0, touchY: 71.94776119402985
+    print(
+        "left: $left, top: $top, right: $right, bottom: $bottom, touchX: $touchX, touchY: $touchY");
+
+    return left <= touchX &&
+        touchX <= right &&
+        top <= touchY &&
+        touchY <= bottom;
   }
 }
