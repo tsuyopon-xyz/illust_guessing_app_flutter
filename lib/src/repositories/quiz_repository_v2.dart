5891c3aae3e8730672105ea49bc6d5c69d481989
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-03-10 12:58:38 +0900

    Refactored quiz_repository_v2

--- lib/src/repositories/quiz_repository_v2.dart
+++ lib/src/repositories/quiz_repository_v2.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:illust_guessing_app/src/models/chapter.dart';
 import 'package:sqflite/sqflite.dart' show Database;
@@ -45,51 +44,13 @@ class QuizRepositoryV2 implements IRepository<QuizV2> {
   @override
   Future<List<QuizV2>> findAll({bool includes = false}) async {
     if (includes) {
-      var records = await db.rawQuery('''
-          SELECT
-            q.id as quiz_id,
-            q.title,
-            c.id as chapter_id,
-            c.sort_order,
-            c.hint,
-            c.question,
-            c.comment,
-            c.correct_area_x,
-            c.correct_area_y,
-            c.correct_area_width,
-            c.correct_area_height
-          FROM
-            $quizzesTableName AS q
-          INNER JOIN
-            $chaptersTableName AS c
-          ON
-            q.id = c.quiz_id;
-          ''');
-
+      var joinedRecords = await db.rawQuery(_buildJoinQuery());
       var recoredsMapByQuizId =
-          records.groupListsBy((record) => record['quiz_id']);
+          joinedRecords.groupListsBy((record) => record['quiz_id']);
 
       List<QuizV2> quizzes = [];
       recoredsMapByQuizId.forEach((quizId, records) {
-        List<Chapter> chapters = records.map((r) {
-          return Chapter(
-            id: r['chapter_id'] as int,
-            quizId: r['quiz_id'] as int,
-            sortOrder: r['sort_order'] as int,
-            hint: r['hint'] as String,
-            question: r['question'] as String,
-            comment: r['comment'] as String,
-            correctAreaX: r['correct_area_x'] as int,
-            correctAreaY: r['correct_area_y'] as int,
-            correctAreaWidth: r['correct_area_width'] as int,
-            correctAreaHeight: r['correct_area_height'] as int,
-          );
-        }).toList();
-        var firstRecord = records.first;
-        QuizV2 quiz = QuizV2(
-            id: firstRecord['quiz_id'] as int,
-            title: firstRecord['title'] as String,
-            chapters: chapters);
+        QuizV2 quiz = _createQuizWithChapters(records);
         quizzes.add(quiz);
       });
 
@@ -120,7 +81,7 @@ class QuizRepositoryV2 implements IRepository<QuizV2> {
   }
 }
 
-String _buildJoinQuery(int? quizId) {
+String _buildJoinQuery([int? quizId]) {
   String onPart = quizId == null ? 'q.id = c.quiz_id' : 'q.id = $quizId';
 
   return '''
